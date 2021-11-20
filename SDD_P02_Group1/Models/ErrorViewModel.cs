cbfc95c6b3536bb19fa60cdf7a4a2774859a54f3
julianee-jk <s10202587@connect.np.edu.sg>
2021-11-20 21:11:16 +0800

    Add project files.

---
+++ SDD_P02_Group1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SDD_P02_Group1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
