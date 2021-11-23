81f0046a67b76f8bbf87da82336626ea1e50d525
julianee-jk <s10202587@connect.np.edu.sg>
2021-11-23 15:13:36 +0800

    Add ConnectionString & Sessions

--- SDD_P02_Group1/Startup.cs
+++ SDD_P02_Group1/Startup.cs
@@ -23,6 +23,15 @@ namespace SDD_P02_Group1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add a default in-memory implementation of distributed cache
+            services.AddDistributedMemoryCache();
+            // Add the session service
+            services.AddSession(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
             services.AddControllersWithViews();
         }
 
@@ -41,11 +50,9 @@ namespace SDD_P02_Group1
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
+            app.UseSession();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
