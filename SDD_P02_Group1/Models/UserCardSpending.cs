55c163659283c3f3f5141ef8eb2a2bfd2a7b4dbe
julianee-jk <s10202587@connect.np.edu.sg>
2022-01-05 12:00:56 +0800

    Add UserCardSpending class

---
+++ SDD_P02_Group1/Models/UserCardSpending.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SDD_P02_Group1.Models
+{
+    public class UserCardSpending
+    {
+        [Required]
+        [Display(Name = "CardSpending ID")]
+        public int CardSpendingID { get; set; }
+
+        public DateTime DateOfTransaction { get; set; }
+
+        public decimal AmountSpent { get; set; }
+
+        [Display(Name = "Total Card Spendings")]
+        public decimal TotalCardAmountSpent { get; set; }
+
+        [Required]
+        [ForeignKey("CardID")]
+        public virtual int UserID { get; set; }
+        public virtual User User { get; set; }
+
+    }
+}
