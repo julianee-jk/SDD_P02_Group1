e51e5d78a68456e61ccc950d3e1e457a28faee0a
Timothy Seow <s10204778@connect.np.edu.sg>
2022-01-21 20:32:29 +0800

    update database setup file

--- SDD_P02_Group1/MoolahDB_Setup.sql
+++ SDD_P02_Group1/MoolahDB_Setup.sql
@@ -13,20 +13,37 @@ GO
 
 
 IF EXISTS (SELECT * FROM sysobjects 
-  WHERE id = object_id('dbo.AccountHolder') AND sysstat & 0xf = 3)
-  DROP TABLE dbo.AccountHolder
+  WHERE id = object_id('dbo.AccountUser') AND sysstat & 0xf = 3)
+  DROP TABLE dbo.AccountUser
 GO
 
 IF EXISTS (SELECT * FROM sysobjects 
-  WHERE id = object_id('dbo.JointAccountInfo') AND sysstat & 0xf = 3)
-  DROP TABLE dbo.JointAccountInfo
+  WHERE id = object_id('dbo.UserAsset') AND sysstat & 0xf = 3)
+  DROP TABLE dbo.UserAsset
 GO
 
 IF EXISTS (SELECT * FROM sysobjects 
-  WHERE id = object_id('dbo.AccountVerification') AND sysstat & 0xf = 3)
-  DROP TABLE dbo.AccountVerification
+  WHERE id = object_id('dbo.UserCard') AND sysstat & 0xf = 3)
+  DROP TABLE dbo.UserCard
 GO
 
+IF EXISTS (SELECT * FROM sysobjects 
+  WHERE id = object_id('dbo.UserCardSpending') AND sysstat & 0xf = 3)
+  DROP TABLE dbo.UserCardSpending
+GO
+
+IF EXISTS (SELECT * FROM sysobjects 
+  WHERE id = object_id('dbo.UserLiability') AND sysstat & 0xf = 3)
+  DROP TABLE dbo.UserLiability
+GO
+
+IF EXISTS (SELECT * FROM sysobjects 
+  WHERE id = object_id('dbo.UserWeeklySpending') AND sysstat & 0xf = 3)
+  DROP TABLE dbo.UserWeeklySpending
+
+IF EXISTS (SELECT * FROM sysobjects 
+WHERE id = object_id('dbo.UserSpendingRecord') AND sysstat & 0xf = 3)
+DROP TABLE dbo.UserSpendingRecord
 
 /*** Create tables ***/
 
@@ -92,23 +109,43 @@ CREATE TABLE dbo.UserWeeklySpending
 )
 GO
 
-CREATE TABLE dbo.AssetChanges
+CREATE TABLE dbo.UserCard
 (
-	UserID				int				NOT NULL,
-	AssetID				int				NOT NULL,
-	Timestamp			VARCHAR(100)	NOT NULL,
-	AssetTypeOld		varchar(50)		NULL,
-	AssetTypeNew		varchar(50)		NULL,
-	AssetDescOld		varchar(200)	NULL,
-	AssetDescNew		varchar(200)	NULL,
-	CurrentValueOld		money			NULL,
-	CurrentValueNew		money			NULL,
-
-	CONSTRAINT PK_AssetChanges PRIMARY KEY NONCLUSTERED (UserID, AssetID, Timestamp),
-
+	CardID			int				NOT NULL IDENTITY,
+	CardName		varchar(50)		NOT NULL,
+	CardType		varchar(50)		NOT NULL,
+	CardDesc		varchar(200)	NULL,
+	UserID				int			NOT NULL,
+	CONSTRAINT PK_UserCard PRIMARY KEY NONCLUSTERED (CardID),
+	CONSTRAINT FK_UserCard_UserID FOREIGN KEY (UserID)
+    REFERENCES dbo.AccountUser(UserID)
 )
 GO
 
+CREATE TABLE dbo.UserCardSpending
+(
+	CardSpendingID		int			NOT NULL IDENTITY,
+	DateOfTransaction	datetime	NOT NULL,
+	AmountSpent			money		NOT NULL,
+	CardID				int			NOT NULL,
+	CONSTRAINT PK_UserCardSpending PRIMARY KEY NONCLUSTERED (CardSpendingID),
+	CONSTRAINT FK_UserCardSpending_CardID FOREIGN KEY (CardID)
+	REFERENCES dbo.UserCard(CardID)
+)
+GO
+
+CREATE TABLE dbo.UserSpendingRecord
+(
+	RecordID			int				NOT NULL IDENTITY,
+	DateOfTransaction	datetime		NOT NULL,
+	CategoryOfSpending	varchar(50)		NOT NULL,
+	AmountSpent			money			NOT NULL,
+	UserID				int				NOT NULL,
+	CONSTRAINT PK_UserSpendingRecord PRIMARY KEY NONCLUSTERED (RecordID),
+	CONSTRAINT FK_UserSpendingRecord_UserID FOREIGN KEY (UserID)
+	REFERENCES dbo.AccountUser(UserID)
+)
+GO
 
 /*** Dummy Values ***/
 
@@ -116,11 +153,11 @@ GO
 INSERT INTO AccountUser VALUES ('John','john123','john@gmail.com')
 INSERT INTO AccountUser VALUES ('Andy','andy123','andy@outlook.com')
 
-INSERT INTO UserAsset VALUES ('Apple Inc','Stocks','why',$100,$120,NULL,1)
-INSERT INTO UserAsset VALUES ('Microsoft Corp','Stocks','why',$200,$190,NULL,1)
+INSERT INTO UserAsset VALUES ('Apple Inc','Stocks',NULL,$100,$120,NULL,1)
+INSERT INTO UserAsset VALUES ('Microsoft Corp','Stocks',NULL,$200,$190,NULL,1)
 
-INSERT INTO UserAsset VALUES ('Tesla Inc','Stocks','why',$500,$550,NULL,2)
-INSERT INTO UserAsset VALUES ('Nvidia Corp','Stocks','why',$300,$270,NULL,2)
+INSERT INTO UserAsset VALUES ('Tesla Inc','Stocks',NULL,$500,$550,NULL,2)
+INSERT INTO UserAsset VALUES ('Nvidia Corp','Stocks',NULL,$300,$270,NULL,2)
 
 INSERT INTO UserWeeklySpending VALUES ('2021/11/01',$50,$30,$100,$15,$79,$230,$170,$647,1)
 INSERT INTO UserWeeklySpending VALUES ('2021/11/01',$10,$7,$10,$9,$21,$47,$84,$188,2)
@@ -131,5 +168,18 @@ INSERT INTO UserLiability VALUES ('Phone Bill','Bills','Phone plan charged by ph
 INSERT INTO UserLiability VALUES ('64 inch TV Installment','Purchase','24 months installment plan for television',52,'2021-12-31','Monthly',24,2)
 INSERT INTO UserLiability VALUES ('Housing Payments','Purchase','2 years till payments are fully paid',370,'2021-12-31','Yearly',2,2)
 
-SELECT * FROM UserAsset
+INSERT INTO UserCard VALUES ('POSB Everyday Card','Debit','Making a purchase everyday will give 2% rebate',1)
+INSERT INTO UserCard VALUES ('OCBC FRANK Card','Credit','Spending at affiliated stores will grant 5% rebate',2)
 
+INSERT INTO UserCardSpending VALUES ('2021-12-12',300,1)
+INSERT INTO UserCardSpending VALUES ('2022-01-01',888,2)
+
+INSERT INTO UserSpendingRecord VALUES ('2021-10-10','Food',72,1)
+INSERT INTO UserSpendingRecord VALUES ('2021-11-11','Clothes',184,1)
+INSERT INTO UserSpendingRecord VALUES ('2022-01-04','Clothes',145,2)
+
+/*
+SELECT * FROM UserCard
+SELECT * FROM UserCardSpending
+SELECT * FROM UserSpendingRecord
+*/
